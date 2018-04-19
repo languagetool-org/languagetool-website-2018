f6a13c280a3b6e5d6185e344c55b2909c9532e88
Daniel Naber <naber@danielnaber.de>
2018-04-19 14:07:21 +0200

    syntax + format fix

--- resources/views/support.blade.php
+++ resources/views/support.blade.php
@@ -14,12 +14,12 @@
             <ul>
                     <li style="font-size: large"><a href="https://forum.languagetool.org/"><strong>Post to our forum</strong></a> - this is the easiest way of contacting us</li>
                     <li>If you absolutely don't want to post to public forums or mailing lists, contact the maintainers directly:
-                            <ul>
-                                    <li><a href="http://www.danielnaber.de">Daniel Naber</a> - contact by sending an email to
-                                            <span style="color:#777777">naber <span>a&#116;</span> danielnaber<span>.</span>de</span></li>
-                                    <li><a href="http://marcinmilkowski.pl">Marcin Miłkowski</a> - contact by using his
-                                            <a href="http://marcinmilkowski.pl/en/contact-me">contact form</a></li>
-                            </ul>
+                        <ul>
+                            <li><a href="http://www.danielnaber.de">Daniel Naber</a> - contact by sending an email to
+                                    <span style="color:#777777">naber <span>a&#116;</span> danielnaber<span>.</span>de</span></li>
+                            <li><a href="http://marcinmilkowski.pl">Marcin Miłkowski</a> - contact by using his
+                                    <a href="http://marcinmilkowski.pl/en/contact-me">contact form</a></li>
+                        </ul>
                     </li>
                     <li><strong><a href="../contact/newsletter.php">Announcement mailing list</a></strong> - low traffic,
                             you will only receive emails about new LanguageTool releases and major new features</li>
@@ -27,7 +27,6 @@
 
         </div>
             
-            </div>
     </div>
 
 @endsection
