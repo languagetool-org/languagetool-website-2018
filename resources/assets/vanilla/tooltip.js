a148f36d546be6231674b01321e4af3eed38d1ce
Nikita Eprev <neprev@ronasit.com>
2018-04-20 12:44:22 +0600

    #9: Deleted unused

--- resources/assets/vanilla/tooltip.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-    $('.explainFeature').tooltipster({
-        delay: 10,
-    	theme: ['tooltipster-shadow','tooltipster-shadow-customized'],
-    	contentCloning: true,
-    	maxWidth: 400,
-    	trigger: 'hover'
-    });
-});
