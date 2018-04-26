07e5d76cdd4c48b1f3ec0dcbeabb4bd38d0c2fb9
Daniel Naber <naber@danielnaber.de>
2018-04-26 08:53:27 +0200

    fix site id; use exactly same piwik code as with old design

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -188,12 +188,12 @@ if (isset($_SERVER['QUERY_STRING']) && strpos($_SERVER['QUERY_STRING'], 'testmod
     (function() {
         var u=(("https:" == document.location.protocol) ? "https" : "http") + "://openthesaurus.stats.mysnip-hosting.de/";
         _paq.push(['setTrackerUrl', u+'piwik.php']);
-        _paq.push(['setSiteId', '2']);
+        _paq.push(['setSiteId', 2]);
         var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0]; g.type='text/javascript';
         g.defer=true; g.async=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
     })();
 </script>
-<noscript><p><img src="http://openthesaurus.stats.mysnip-hosting.de/piwik.php?idsite=11" style="border:0;" alt="" /></p></noscript>
+<noscript><p><img src="http://openthesaurus.stats.mysnip-hosting.de/piwik.php?idsite=2" style="border:0;" alt="" /></p></noscript>
 <!-- End Piwik Code -->
 <!-- Script for Register Modal -->
 <script>
