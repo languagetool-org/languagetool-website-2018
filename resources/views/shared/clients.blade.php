7cb3497b6b33fe5ef954cd8e77b3f4a17e42e327
Daniel Naber <naber@danielnaber.de>
2018-05-17 23:11:29 +0200

    add customer logos

--- resources/views/shared/clients.blade.php
+++ resources/views/shared/clients.blade.php
@@ -17,30 +17,14 @@
 						<div class="section-clients-item glide__slide">
 							<img width="150" src="/images/references/iserv.png" alt="IServ GmbH">
 						</div>
-						<div class="section-clients-item glide__slide">
-							<img src="/images/references/korrekturen_de.png" alt="korrekturen.de">
-						</div>
-						<div class="section-clients-item glide__slide">
-							<img width="210" src="/images/references/papyrus.png" alt="Papyrus Autor">
-						</div>
-						<div class="section-clients-item glide__slide">
-							<img src="/images/references/rsp24.png" alt="rechtschreibpruefung24.de">
-						</div>
-						<div class="section-clients-item glide__slide">
-							<img width="150" src="/images/references/iserv.png" alt="IServ GmbH">
-						</div>
+                        <div class="section-clients-item glide__slide">
+                            <img width="280" src="/images/references/brandwatch.png" alt="brandwatch">
+                        </div>
+                        <div class="section-clients-item glide__slide">
+                            <img width="220" src="/images/references/invaris.png" alt="INVARIS Informationssysteme GmbH">
+                        </div>
 	                </ul>
 	            </div>
-				<!--
-				<div class="section-clients-item">
-					<img width="280" src="/images/references/brandwatch.png" alt="brandwatch">
-				</div>
-				-->
-				<!--
-				<div class="section-clients-item">
-					<img width="220" src="/images/references/invaris.png" alt="INVARIS Informationssysteme GmbH">
-				</div>
-				-->
 			</div>
 
 			<button class="section-clients-items-control section-clients-items-control-left" data-glide-dir="<">
