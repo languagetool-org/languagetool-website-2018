fd1506e374a5666c612253e824d2e8f23ffb55a6
Daniel Naber <naber@danielnaber.de>
2018-07-04 14:08:53 +0200

    small wording updates

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -3,7 +3,7 @@
 return [
     'language_hint' => 'Seems like you might be interested in LanguageTool for :lang?',
     'language_link' => 'Take me there.',
-    'title' => 'Style and Grammar Checker',
+    'title' => 'Spell and Grammar Checker',
     'description' => 'LanguageTool is a proofreading software for English, German, Spanish, Russian, and more than 20 other languages.',
     'cookie_hint' => 'This website uses cookies for a better user experience and to show ads.',
     'cookie_more' => 'More / privacy policy',
