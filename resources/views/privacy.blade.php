81f0dbc6d8f328e3c0b1f4fdfb4924dfa1d4158f
Daniel Naber <naber@danielnaber.de>
2018-12-05 14:19:25 +0100

    small wording change

--- resources/views/privacy.blade.php
+++ resources/views/privacy.blade.php
@@ -51,7 +51,7 @@
                             <ul>
                                 <li>Datum und Zeit, Länge des Textes, Sprache des Textes, Dauer der Verarbeitung, Anzahl
                                     der erkannten Fehler (aber nicht den fehlerhaften Text, außer Sie haben dem zugestimmt),</li>
-                                <li>die Seite, von der die Anfrage abgeschickt wurde (normalerweise languagetool.org),</li>
+                                <li>die Seite, von der die Anfrage abgeschickt wurde (z.B. languagetool.org),</li>
                                 <li>interne Fehler (wenn z.&nbsp;B. das Browser Add-on auf den zu prüfenden Text nicht zugreifen kann),</li>
                                 <li>nur bei einer Browser-Add-on-Deinstallation wird übertragen: die Anzahl der Nutzungen des Add-ons
                                     und die URL der Seite, auf der das Add-on zuletzt benutzt wurde</li>
@@ -399,7 +399,7 @@
                         <li>To improve our proofreading service, we keep a log of the following information:
                             <ul>
                                 <li>date and time, length of submitted text, text language, processing time on server, number of detected errors (but not the actual errors unless you have agreed to that),</li>
-                                <li>the page on which you submitted the request (referrer),</li>
+                                <li>the page on which you submitted the request (referrer, e.g. languagetool.org),</li>
                                 <li>internal errors that occur (e.g. the browser add-on not being able to access the text to be checked),</li>
                                 <li>only transferred when you uninstall the browser add-on: the number of times you used the add-on
                                     and the URL of the page where the add-on was last used</li>
