2cbd766555a0e155a9f203e85c4d46f925659667
Daniel Naber <naber@danielnaber.de>
2019-01-30 18:08:37 +0100

    new job ad

---
+++ resources/views/job/rule_developer_de.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+@section('title', 'Teilzeit-Job: Regel-Entwickler / Redakteur')
+
+@section('content')
+    <div id="textContent" class="content-section" style="text-align: left">
+
+        <div class="container">
+
+            <p style="text-align: right">Date: 2019-01-30</p>
+            
+            <h1>Regel-Entwickler / Redakteur (m/w/d)</h1>
+
+            <!--
+            <p style="background-color: rgba(255,227,57,0.6); padding: 5px">
+                This position has been staffed.
+            </p> -->
+            
+            <p>Die LanguageTooler GmbH ist die Firma hinter languagetool.org, languagetoolplus.com und
+                <a href="https://openthesaurus.de">openthesaurus.de</a>. Auf unseren Websites prüfen
+                wir jeden Tag mehr als 500.000 mal Texte vollautomatisch. Wir suchen einen</p>
+
+            <h2 style="text-align: center">Regel-Entwickler / Redakteur (m/w/d),<br>8 bis 20 Stunden/Woche</h2>
+
+            <p><b>Deine Aufgaben</b></p>
+
+            <ul>
+                <li>neue Regeln zur automatischen Fehlererkennung schreiben</li>
+                <li>Deine neuen Regeln und die bestehenden Regeln testen und verbessern</li>
+                <li>Fehlermeldungen und Hinweise schreiben, die unseren Nutzern Fehler erklären und sprachliche Regeln nahebringen</li>
+            </ul>
+
+            <p><b>Was wir erwarten</b></p>
+
+            <ul>
+                <li>Deutsch auf Muttersprachler-Niveau</li>
+                <li>hohe Technikaffinität, um die Syntax zu lernen, mit der Regeln geschrieben werden</li>
+            </ul>
+
+            <p><b>Was wir bieten</b></p>
+
+            <ul>
+                <!--<li>Startgehalt von €../h</li>-->
+                <li>freie Wahl des Arbeitsortes, zu Hause oder bei uns im Büro</li>
+                <li>freie Zeiteinteilung</li>
+                <li>arbeite mit an einer Software, die jeden Tag von hunderttausenden Menschen benutzt wird</li>
+            </ul>
+
+            <p>Interesse? Sende Deine Fragen oder direkt Deine Bewerbung per
+                <script type="text/javascript">
+                    <!--
+                    var firstPart = "daniel.naber";
+                    var lastPart = "languagetool.org";
+                    document.write("<a href='mail" + "to:" + firstPart + "@" + lastPart + "'>E-Mail an Daniel Naber<" + "/a>");
+                    // -->
+                </script>.<noscript>(bitte Javascript aktivieren, um die E-Mail-Adresse zu sehen)</noscript>
+
+            <p>LanguageTooler GmbH<br>
+                Karl-Liebknecht-Str. 21/22<br>
+                14482 Potsdam
+            
+        </div>
+
+    </div>
+@endsection
