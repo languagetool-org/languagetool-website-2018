000e58b29130e88980aaad29a59946c040fe42b1
Daniel Naber <naber@danielnaber.de>
2019-05-15 09:45:35 +0200

    prepare migration to a new matomo instance (thus tracking to two instances for a short time)

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -305,3 +305,20 @@ $(document).ready(function() {
 </script>
 <noscript><p><img src="https://openthesaurus.stats.mysnip-hosting.de/piwik.php?idsite=2" style="border:0;" alt="" /></p></noscript>
 <!-- End Piwik Code -->
+
+
+<!-- Matomo -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//analytics.languagetoolplus.com/piwik/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', '2']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<!-- End Matomo Code -->
