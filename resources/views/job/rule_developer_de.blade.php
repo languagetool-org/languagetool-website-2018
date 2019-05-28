a9682bce425af0fa4ac82b7c5e408339d4ad2259
Daniel Naber <naber@danielnaber.de>
2019-05-28 14:49:55 +0200

    [de] update job ad

--- resources/views/job/rule_developer_de.blade.php
+++ resources/views/job/rule_developer_de.blade.php
@@ -1,32 +1,42 @@
 @extends('layouts.app')
 
-@section('title', 'Teilzeit-Job: Regel-Entwickler / Redakteur (m/w/d)')
+@section('title', 'Job: Regel-Entwickler / Redakteur (m/w/d)')
 
 @section('content')
     <div id="textContent" class="content-section" style="text-align: left">
 
         <div class="container">
 
-            <p style="text-align: right">Datum: 31.01.2019</p>
+            <p style="text-align: right">Datum: 28.05.2019</p>
             
             <h1>Regel-Entwickler / Redakteur (m/w/d)</h1>
 
+            <!--
             <p style="background-color: rgba(255,227,57,0.6); padding: 5px">
                 Diese Position ist bereits vergeben
             </p>
+            -->
             
             <p>Die LanguageTooler GmbH ist die Firma hinter languagetool.org, languagetoolplus.com und
                 <a href="https://openthesaurus.de">openthesaurus.de</a>. Auf unseren Websites prüfen
-                wir jeden Tag mehr als 500.000 mal Texte vollautomatisch. Wir suchen einen</p>
+                wir jeden Tag mehr als 500.000 Texte vollautomatisch. Wir suchen einen</p>
 
-            <h2 style="text-align: center">Regel-Entwickler / Redakteur (m/w/d),<br>8 bis 20 Stunden/Woche</h2>
+            <h2 style="text-align: center">Regel-Entwickler / Redakteur (m/w/d),<br>20 bis 40 Stunden/Woche</h2>
 
             <p><b>Deine Aufgaben</b></p>
+            
+            <p>Du arbeitest daran, LanguageTool nachhaltig zu verbessern und so zu erweitern,
+            dass es unseren Nutzern hilft, korrekte und stilistisch gute Texte zu verfassen.</p>
 
             <ul>
-                <li>neue Regeln zur automatischen Fehlererkennung schreiben</li>
-                <li>Deine neuen Regeln und die bestehenden Regeln testen und verbessern</li>
-                <li>Fehlermeldungen und Hinweise schreiben, die unseren Nutzern Fehler erklären und sprachliche Regeln nahebringen</li>
+                <li>Du planst und priorisierst Erweiterungen von LanguageTool, um sie dann selbst umzusetzen</li>
+                <li>Du schreibst Regeln, die unseren Nutzern stilistische Verbesserungen im Text vorschlagen</li>
+                <li>Du schreibst Regeln, die Fehler erkennen</li>
+                <li>Du schreibst Regeln, die Nicht-Muttersprachlern helfen, besser Deutsch zu schreiben</li>
+                <li>Du testest und verbesserst Deine neuen und die bestehenden Regeln</li>
+                <li>Du schreibst Fehlermeldungen und Hinweise, die unseren Nutzern Fehler erklären, sprachliche Regeln nahebringen,
+                    und Tipps für besseren Stil geben</li>
+                <li>Du betreust unsere anderen Regel-Entwickler</li>
             </ul>
 
             <p><b>Was wir erwarten</b></p>
@@ -34,15 +44,20 @@
             <ul>
                 <li>Deutsch auf Muttersprachler-Niveau</li>
                 <li>hohe Technikaffinität, um die Syntax zu lernen, mit der Regeln geschrieben werden</li>
+                <li>Begeisterung für Sprache</li>
+                <li>gerne auch sehr gute Englischkenntnisse</li>
+                <li>gerne auch ein frisch abgeschlossenes Studium der Germanistik, Linguistik, Computerlinguistik, Informatik, o.ä. -
+                    Berufserfahrung ist keine Voraussetzung</li>
             </ul>
 
             <p><b>Was wir bieten</b></p>
 
             <ul>
                 <!--<li>Startgehalt von €../h</li>-->
-                <li>freie Wahl des Arbeitsortes, zu Hause oder bei uns im Büro</li>
-                <li>freie Zeiteinteilung</li>
-                <li>arbeite mit an einer Software, die jeden Tag von hunderttausenden Menschen benutzt wird</li>
+                <li>Du arbeitest in einem kleinen, flexiblen Team</li>
+                <li>Du kannst regelmäßig von zu Hause zu arbeiten oder in unserem Büro in Potsdam-Babelsberg (in der Nähe der S-Bahn)</li>
+                <li>Du arbeitest mit an einer Software, die jeden Tag von hunderttausenden Menschen benutzt wird</li>
+                <li>Deine Ideen kannst Du direkt umsetzen und sie sind spätestens am nächsten Tag schon online</li>
             </ul>
 
             <p>Interesse? Sende Deine Fragen oder direkt Deine Bewerbung per
