4bc00f943b0ee9565b417cee1aaed15b29d9613a
Daniel Naber <naber@danielnaber.de>
2019-06-13 18:10:26 +0200

    fix tracking

--- resources/views/shared/footer.blade.php
+++ resources/views/shared/footer.blade.php
@@ -286,16 +286,16 @@ $(document).ready(function() {
 
 <!-- Matomo -->
 <script type="text/javascript">
-  var _paq = window._paq || [];
+  var _paq = _paq || [];
   /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
   _paq.push(['trackPageView']);
   _paq.push(['enableLinkTracking']);
   (function() {
     var u="//analytics.languagetoolplus.com/matomo/";
-    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
     _paq.push(['setSiteId', '2']);
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
   })();
 </script>
 <!-- End Matomo Code -->
