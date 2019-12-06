d67651a1e28eb3c271c9d6e6e1cbdb81d82f593a
Daniel Naber <naber@danielnaber.de>
2019-12-06 14:41:52 +0100

    add structured data

--- resources/views/job/sea_de.blade.php
+++ resources/views/job/sea_de.blade.php
@@ -1,6 +1,34 @@
 @extends('layouts.app')
 
 @section('title', 'Job: Online Marketing und SEA-Manager (m/w/d)')
+@section('noindex')
+<script type="application/ld+json">
+    {
+        "@context" : "https://schema.org/",
+        "@type" : "JobPosting",
+        "title" : "Online Marketing und SEA-Manager (m/w/d)",
+        "description" : "<p>Die LanguageTooler GmbH ist die Firma hinter <a href='https://languagetool.org/'>languagetool.org</a>,                 <a href='https://languagetoolplus.com'>languagetoolplus.com</a> und                 <a href='https://openthesaurus.de'>openthesaurus.de</a>. Auf unseren Websites prüfen                 wir jeden Tag mehr als 500.000 Texte vollautomatisch. Wir suchen einen</p>              <h2 style='text-align: center'>Online Marketing und SEA-Manager (m/w/d),<br>20 bis 40 Stunden/Woche</h2>              <p><b>Deine Aufgaben</b></p>                          <ul>                 <li>Definition von Zielgruppen</li>                 <li>Planung und Umsetzung von Online-Marketing-Kampagnen (z. B. Google Ads) für die verschiedenen Zielgruppen</li>                 <li>Selbständige Erstellung von Landing Pages für deine Kampagnen (nach Vorgaben unseres Designers)</li>                 <li>Kontinuierliche Performanceüberwachung bzw. Reporting aller Maßnahmen</li>             </ul>              <p><b>Was wir erwarten</b></p>              <ul>                 <li>Du hast ausgeprägte analytische Fähigkeiten, ein hervorragendes Zahlen- und Datenverständnis, sowie eine strukturierte Vorgehensweise</li>                 <li>Berufserfahrung im Online-Marketing oder SEA, insbesondere mit Google Ads</li>                 <li>Erfahrung mit der Erstellung von Landing Pages</li>                 <li>Eigeninitiative und selbständige Arbeitsweise</li>             </ul>              <p><b>Was wir bieten</b></p>              <ul>                 <li>Du arbeitest in einem kleinen, flexiblen Team</li>                 <li>Flexible Arbeitszeiten und die Möglichkeit, regelmäßig im Home Office zu arbeiten</li>                 <li>Du arbeitest mit an einer Software, die jeden Tag von hunderttausenden Menschen benutzt wird</li>                 <li>Deine Ideen kannst du direkt umsetzen</li>                 <li>Ein Büro im Zentrum von Potsdam-Babelsberg (in der Nähe der S-Bahn, sehr gut auch aus Berlin zu erreichen)</li>                 <li>Unbefristeter Arbeitsvertrag</li>             </ul>              <p>Interesse? Sende deine Fragen oder direkt deine Bewerbung per E-Mail an daniel.naber@languagetool.org.              <p>LanguageTooler GmbH<br>                 Karl-Liebknecht-Str. 21/22<br>               ",
+        "identifier": {
+            "@type": "PropertyValue",
+            "name": "LanguageTooler GmbH",
+            "value": "20191115-sea"
+        },
+        "datePosted" : "2019-11-15",
+        "employmentType": ["FULL_TIME", "PART_TIME"],
+        "hiringOrganization" : {
+            "@type" : "Organization",
+            "name" : "LanguageTooler GmbH",
+            "sameAs" : "https://languagetool.org/",
+            "logo" : "https://languagetool.org/images/logo.svg"
+        },
+        "applicantLocationRequirements": {
+            "@type": "Country",
+            "name": "Germany"
+        },
+        "jobLocationType": "TELECOMMUTE"
+    }
+</script>
+@stop
 
 @section('content')
     <div id="textContent" class="content-section" style="text-align: left">
