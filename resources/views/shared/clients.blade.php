d1fb6d53ee88262634e31b7af22d2a34b65e6380
Daniel Naber <naber@danielnaber.de>
2020-04-09 10:56:23 +0200

    update reference customers

--- resources/views/shared/clients.blade.php
+++ resources/views/shared/clients.blade.php
@@ -8,18 +8,12 @@
                         <?php
                         $clients = [
                                 'Bertelsmann Stiftung' => 'bertelsmann_stiftung.png',
+                                'DER SPIEGEL' => 'spiegel.png',
                                 'Blinkist - Blinks Labs GmbH' => 'blinkist.png',
                                 'audibene GmbH' => 'audibene.png',
                                 'lokalise.co' => 'lokalise.png',
-                                'H&H Communication Lab GmbH' => 'comlab.png',
                                 'Dimelo' => 'dimelo.png',
-                                //'translate5.net' => 'translate5.png',
-                                'korrekturen.de' => 'korrekturen_de.png',
                                 'maxcluster GmbH' => 'maxcluster.png',
-                                //'rechtschreibpruefung24.de' => 'rsp24.png',
-                                'brandwatch' => 'brandwatch.png',
-                                //'INVARIS Informationssysteme GmbH' => 'invaris.png',
-                                //'TechScribe' => 'techscribe.png',
                                 'icare Sales & Services' => 'icare.png',
                         ];
                         shuffle($clients);
